36079a722c9e1e59af2943a974039350653cef2f
quickly520@126.com <quickly520@126.com>
2017-05-24 17:10:52 +0800

    添加获取图片的代码

--- getImg.py
+++ getImg.py
@@ -1,12 +1,33 @@
-# coding:utf-8
+# coding:UTF-8
 
-import urllib
-import sys
+import urllib.request
 import re
-import os
-from importlib import reload
 
 
 def getHtml(url):
-    reload(sys)
-    return urllib.urlppen(url).read()
+    return urllib.request.urlopen(url).read().decode("UTF-8")
+
+
+def get_Html(url):
+    return urllib.request.urlopen(url).read()
+
+
+def writeToFile(path, name, data):
+    file = open(path + name, 'wb')
+    file.write(data)
+    file.close()
+    print('save img succeed')
+
+
+url = 'http://www.u148.net/article/55557.html'
+page = getHtml(url)
+reg = r'src="(.+?\.jpg)"'
+imgre = re.compile(reg)
+img_list = re.findall(imgre, page)
+
+path = "/Users/lzw/Desktop/mubai/workspace/python/weather/img/"
+for i, img in enumerate(img_list):
+    if i == 0:
+        continue
+    else:
+        writeToFile(path, str(i) + ".jpg", get_Html(img))
